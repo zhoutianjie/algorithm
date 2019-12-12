5214ea790cd92d2c63147e1678d7c717506b3382
tianjie.zhou <tianjie.zhou@aispeech.com>
2019-12-12 10:43:06 +0800

    LRU缓存机制 双链表+HashMap

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,15 @@ import threads.lettcode_1116.ZeroEvenOdd;
 public class Main {
 
     public static void main(String[] args){
-       System.out.println(0<<10);
+        LRUCache cache = new LRUCache(2);
+        cache.put(1,1);
+        cache.put(2,2);
+        System.out.println(cache.get(1));
+        cache.put(3,3);
+        System.out.println(cache.get(2));
+        cache.put(4,4);
+        System.out.println(cache.get(1));
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
     }
 }
